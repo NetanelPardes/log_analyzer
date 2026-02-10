81088e442bf2be6dee2cf6493dcb31cb91916c7d
Netanel Pardes <sendi8475@gmail.com>
2026-02-10 09:42:33 +0200

    Adding a function that identifies all addresses with a sensitive port

--- analyzer.py
+++ analyzer.py
@@ -1,3 +1,6 @@
 
 def external_addresses(data):
     return [line for line in data if not line[1].startswith(('10.' ,'192.168'))]
+
+def filtering_by_sensitive_port(data):
+    return [line for line in data if line[3] in ('22', '3389' , '23')]
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from reader import read_network_file
-from analyzer import external_addresses
+from analyzer import external_addresses, filtering_by_sensitive_port
 
 def main():
     my_path = 'C:\\Users\\sendi\\PycharmProjects\\PythonProject1\\log_analyzer\\log_analyzer\\network_traffic.log'
     network_line = read_network_file(my_path)
     external_IP_addresses = external_addresses(network_line)
-    
+    # for ip in external_IP_addresses:
+    #     print(ip)
+    sensitiveport = filtering_by_sensitive_port(network_line)
+    # for port in sensitiveport:
+    #     print(port)
+
 if __name__ == "__main__":
     main()
