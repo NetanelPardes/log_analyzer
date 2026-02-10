da093d5dfa793ea9b9a2b085418bdd04aa4103ea
Netanel Pardes <sendi8475@gmail.com>
2026-02-10 10:03:20 +0200

    Adding two functions, one filters only those with a view greater than 5000, and the other labels all messages by size.

--- analyzer.py
+++ analyzer.py
@@ -4,3 +4,9 @@ def external_addresses(data):
 
 def filtering_by_sensitive_port(data):
     return [line for line in data if line[3] in ('22', '3389' , '23')]
+
+def filter_by_size(data,size):
+    return [line for line in data if int(line[5]) > size]
+
+def tag_traffic_by_size(data , size):
+    return [line + ["LARGE"] if int(line[5]) > size else line + ["NORMAL"] for line in data]
--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 from reader import read_network_file
-from analyzer import external_addresses, filtering_by_sensitive_port
+from analyzer import *
 
 def main():
     my_path = 'C:\\Users\\sendi\\PycharmProjects\\PythonProject1\\log_analyzer\\log_analyzer\\network_traffic.log'
     network_line = read_network_file(my_path)
+
     external_IP_addresses = external_addresses(network_line)
+
     # for ip in external_IP_addresses:
     #     print(ip)
+
     sensitiveport = filtering_by_sensitive_port(network_line)
+
     # for port in sensitiveport:
     #     print(port)
 
+    greater_than_5000 = filter_by_size(network_line,5000)
+
+    # for port in greater_than_5000:
+    #     print(port)
+
+    tag_lines = tag_traffic_by_size(network_line,5000)
+
+    for port in tag_lines:
+        print(port)
 if __name__ == "__main__":
     main()
